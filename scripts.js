98ef559a887b4be7d649e9db660d2955af6ae276
samlarson <slarson207@gmail.com>
2017-11-13 15:37:29 -0500

    Added auxiliary js test, updated main page info, added borders to main sections

--- scripts.js
+++ scripts.js
@@ -1,8 +1,18 @@
-var lineDrawing = anime({
+var lineDrawing1 = anime({
   targets: '#lineDrawing .lines path',
   strokeDashoffset: [anime.setDashoffset, 0],
   easing: 'easeInOutSine',
   duration: 2500,
+  delay: function(el, i) { return i * 250 },
+  direction: 'alternate',
+  loop: true
+});
+
+var lineDrawing2 = anime({
+  targets: '#lineDrawing2 .lines path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 1000,
   delay: function(el, i) { return i * 250 },
   direction: 'alternate',
   loop: true
